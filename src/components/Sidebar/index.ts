89018d949a7b3a1746e19dec3c2d5828ec1810c9
Ian Mayo <ian@planetmayo.com>
2025-07-01 13:07:40 +0100

    styling complete

---
+++ src/components/Sidebar/index.ts
@@ -0,0 +1,5 @@
+// Sidebar component index file
+// This file will be used to export any Sidebar-related functionality in the future
+// Currently just serves as an entry point for importing the CSS
+
+import './Sidebar.css'
