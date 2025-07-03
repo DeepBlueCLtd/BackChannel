0299c7513ad2d2a62f3850969370112a547a084d
Ian Mayo <ian@planetmayo.com>
2025-07-03 17:17:57 +0100

    better badge

--- src/components/Badge/index.ts
+++ src/components/Badge/index.ts
@@ -3,7 +3,7 @@
  * This component handles the rendering and state management of the BackChannel icon
  */
 
-import { LitElement, html } from 'lit'
+import { LitElement, html, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 /**
@@ -11,11 +11,38 @@ import { customElement } from 'lit/decorators.js'
  */
 @customElement('bc-badge')
 export class BackChannelBadge extends LitElement {
+  // Badge is hardcoded to top-right position as requested
+
+  /**
+   * CSS styles for the component
+   */
+  static styles = css`
+    :host {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: fixed;
+      z-index: 9999;
+      top: 20px;
+      right: 20px;
+      width: 60px;
+      height: 60px;
+      background-color: #888;
+      border-radius: 8px;
+    }
+
+    div {
+      color: white;
+      font-weight: bold;
+      font-size: 24px;
+    }
+  `
+
   /**
    * Render the component
    */
   render() {
-    return html`<div>[BC]</div>`
+    return html`<div>BC</div>`
   }
 }
 
